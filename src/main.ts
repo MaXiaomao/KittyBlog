0c46007b0b29ab510bc4e72f3fe432985bfa01fc
MaXiaomao <maxiaomaoemail@foxmail.com>
2021-12-23 11:36:38 +0800

    创建项目公用样式文件并挂载至main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,7 @@ import {createApp} from "vue"
 import App from "./App.vue"
 // @ts-ignore
 import Router from "./router/index.ts"
+import "element-plus/theme-chalk/display.css"
+import "./config/main.css"
 
 createApp(App).use(Router).mount("#app")
