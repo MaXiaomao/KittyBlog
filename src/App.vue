75a222b02e5707a5edd2af94bc4bc01e64ff3c8e
MaXiaomao <maxiaomaoemail@foxmail.com>
2021-12-26 10:08:34 +0800

    配置国际化

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,24 @@
 <template>
-	<HeaderComponent />
-	<router-view />
-	<FooterComponent />
+	<el-config-provider :locale="locale">
+		<HeaderComponent />
+		<router-view />
+		<FooterComponent />
+	</el-config-provider>
 </template>
 
 <script lang="ts">
 import {defineComponent} from "vue"
+import {ElConfigProvider} from "element-plus"
+import zhCn from "element-plus/lib/locale/lang/zh-cn"
 import HeaderComponent from "./components/Header/index.vue"
 import FooterComponent from "./components/Footer/index.vue"
 
 export default defineComponent({
 	name: "App",
-	components: {HeaderComponent, FooterComponent},
-	setup() {},
+	components: {ElConfigProvider, HeaderComponent, FooterComponent},
+	setup() {
+		return {locale: zhCn}
+	},
 })
 </script>
 
