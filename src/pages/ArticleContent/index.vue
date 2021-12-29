0db6cc9a72e401351b6d91e5a5e03c54c356e608
MaXiaomao <maxiaomaoemail@foxmail.com>
2021-12-29 15:03:55 +0800

    完成ArticleContent页面基本布局

--- src/pages/ArticleContent/index.vue
+++ src/pages/ArticleContent/index.vue
@@ -1,11 +1,98 @@
 <template>
-	<el-empty description="内容页开发中"></el-empty>
+	<div class="page-main version-heart">
+		<article-component />
+		<self-info-component class="margin-bot" />
+		<div class="comment">
+			<p class="title">共 5 条评论关于 “在Nuxtjs中使用@nuxtjs/axios与vuex的一些心得”</p>
+			<div class="publish-block">
+				<div class="publish-info">
+					<strong>发表评论</strong>
+					<span>昵称、邮箱为必填项，电子邮件地址不会被公开</span>
+				</div>
+				<comment-form-component />
+			</div>
+			<div class="comment-list">
+				<comment-item-component :data="data" />
+			</div>
+		</div>
+	</div>
 </template>
 
-<script>
-export default {
-	name: "index",
-}
+<script lang="ts">
+import {defineComponent, ref} from "vue"
+import ArticleComponent from "../../components/Article/index.vue"
+import SelfInfoComponent from "../../components/SelfInfo/index.vue"
+import CommentFormComponent from "../../components/CommentForm/index.vue"
+import CommentItemComponent from "../../components/CommentItem/index.vue"
+
+export default defineComponent({
+	name: "ArticleContentComponent",
+	components: {ArticleComponent, SelfInfoComponent, CommentFormComponent, CommentItemComponent},
+	setup() {
+		const data = ref({
+			id: 1,
+			name: "小懒",
+			time: "2020-10-12 19:00:00",
+			body: "自建Gravatar加速了啊",
+			reply: "",
+			children: [
+				{
+					id: 2,
+					name: "小茂",
+					time: "2020-10-12 19:00:00",
+					body: "自建Gravatar加速了啊",
+					reply: "",
+				},
+				{
+					id: 3,
+					name: "小茂",
+					time: "2020-10-12 19:00:00",
+					body: "看报错，好像版本有问题，网上搜一下呢",
+					reply: "自建Gravatar加速了啊",
+				},
+				{
+					id: 4,
+					name: "小茂",
+					time: "2020-10-12 19:00:00",
+					body: "自建Gravatar加速了啊",
+					reply: "",
+				},
+			],
+		})
+
+		return {data}
+	},
+})
 </script>
 
-<style scoped></style>
+<style scoped>
+.comment {
+	padding: 15px;
+	border-radius: 4px;
+	background-color: #ffffff;
+}
+.title {
+	margin: 0 0 15px;
+	text-align: center;
+}
+.publish-block {
+	padding: 15px;
+	border-radius: 4px;
+	background-color: #ebeef5;
+}
+.publish-info strong {
+	color: #303133;
+	display: block;
+	font-size: 14px;
+	margin-bottom: 5px;
+	font-weight: normal;
+}
+.publish-info span {
+	color: #909399;
+	display: block;
+	font-size: 12px;
+}
+.margin-bot {
+	margin-bottom: 15px;
+}
+</style>
