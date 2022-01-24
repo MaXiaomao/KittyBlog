5f46560bb222c34285fa19b2d478bc03f1bee187
MaXiaomao <maxiaomaoemail@foxmail.com>
2022-01-24 14:40:53 +0800

    删除远程图片

--- src/components/Banner/index.vue
+++ src/components/Banner/index.vue
@@ -1,35 +1,19 @@
 <template>
 	<div class="banner">
 		<div class="banner-main banner-item">
-			<el-image
-				src="https://upyun.xuanmo.xin/images/20180623145637.jpg"
-				style="width: 100%; height: 100%"
-				fit="cover"
-			></el-image>
+			<el-image src="" style="width: 100%; height: 100%" fit="cover"></el-image>
 			<p>原创 Nuxtjs + WordPress 主题，前后端分离，欢迎体验</p>
 		</div>
 		<div class="banner-item">
-			<el-image
-				src="https://blog.xuanmo.xin/wp-content/uploads/2020/06/18881590731903_.pic_.jpg"
-				style="width: 100%; height: 100%"
-				fit="cover"
-			></el-image>
+			<el-image src="" style="width: 100%; height: 100%" fit="cover"></el-image>
 			<p>我的 form 动态表单组件</p>
 		</div>
 		<div class="banner-item">
-			<el-image
-				src="https://www.xuanmo.xin/wp-content/uploads/2019/07/WechatIMG386.jpg"
-				style="width: 100%; height: 100%"
-				fit="cover"
-			></el-image>
+			<el-image src="" style="width: 100%; height: 100%" fit="cover"></el-image>
 			<p>垃圾分类小妙招-我的小程序</p>
 		</div>
 		<div class="banner-item">
-			<el-image
-				src="https://blog.xuanmo.xin/wp-content/uploads/2020/06/gh_644ee0d14483_258-1.jpg"
-				style="width: 100%; height: 100%"
-				fit="cover"
-			></el-image>
+			<el-image src="" style="width: 100%; height: 100%" fit="cover"></el-image>
 			<p>博客小程序版正式发布</p>
 		</div>
 	</div>
